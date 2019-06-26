233ba9ee7ee7b6e77ed649e19bc1199cb4173401
Jaden Pham <tjaden.pham@gmail.com>
2019-06-26 16:55:45 -0700

    second commit

--- my_project/apps/myapp/urls.py
+++ my_project/apps/myapp/urls.py
@@ -8,8 +8,14 @@ urlpatterns = [
     url(r'^signupp$',views.signupp),
     url(r'^loginp$',views.loginp),
     url(r'^logout$',views.logout),
+    url(r'^add$',views.add),
     url(r'^thelogin$',views.thelogin),
     url(r'^searching$',views.searching),
     url(r'^searching2$',views.searching2),
+    url(r'^addbrew$', views.addbrew), 
+    url(r'^ales$', views.ales), 
+    url(r'^malts$', views.malts), 
+    url(r'^lagers$', views.lagers), 
+    url(r'^stouts$', views.stouts), 
     
     ]
