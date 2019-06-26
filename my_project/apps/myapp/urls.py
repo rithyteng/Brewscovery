66c41fab0393a6467f17ff8f7490962de3a1e5a3
Tengrithy Khling <rithyteng@Tengrithys-MacBook-Pro.local>
2019-06-26 15:51:56 -0700

    Fix`

--- my_project/apps/myapp/urls.py
+++ my_project/apps/myapp/urls.py
@@ -9,9 +9,7 @@ urlpatterns = [
     url(r'^loginp$',views.loginp),
     url(r'^logout$',views.logout),
     url(r'^thelogin$',views.thelogin),
-<<<<<<< HEAD
     url(r'^searching$',views.searching),
     url(r'^searching2$',views.searching2),
-=======
->>>>>>> 1548ee2665fb10a75ff43d3324ed8f848ccf43c1
-]
+    
+    ]
